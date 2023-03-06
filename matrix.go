82d2d3fa572d5df6cbbd10a0f21184c691fddf6a
Oliver <oliver@monoxane.io>
2023-03-06 23:30:46 +1100

    json marshal the matrix state

--- matrix.go
+++ matrix.go
@@ -1,6 +1,42 @@
 package nk
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"sort"
+)
+
+func (matrix Matrix) MarshalJSON() ([]byte, error) {
+	log.Print("marshalling matrix")
+	type res struct {
+		Destinations []*Destination `json:"destinations,omitempty"`
+		Sources      []*Source      `json:"sources,omitempty"`
+	}
+
+	var result res
+
+	for _, dst := range matrix.destinations {
+		if dst != nil {
+			result.Destinations = append(result.Destinations, dst)
+		}
+	}
+
+	sort.Slice(result.Destinations, func(i, j int) bool {
+		return result.Destinations[i].Id < result.Destinations[j].Id
+	})
+
+	for _, src := range matrix.sources {
+		if src != nil {
+			result.Sources = append(result.Sources, src)
+		}
+	}
+
+	sort.Slice(result.Sources, func(i, j int) bool {
+		return result.Sources[i].Id < result.Sources[j].Id
+	})
+
+	return json.Marshal(&result)
+}
 
 func (matrix *Matrix) SetCrosspoint(dst uint16, src uint16) {
 	matrix.mux.Lock()
